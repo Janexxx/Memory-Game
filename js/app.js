8c10db40633ca99822a6b60e301122a7124afe1b
zedfoxus <contact@zedfox.us>
2017-12-05 14:15:09 -0600

    Moved instructions to the top of the file

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 /*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+/*
  * Create a list that holds all of your cards
  */
 const cardSymbol = ['fa-bicycle', 'fa-bicycle', 
@@ -79,18 +90,6 @@ function initGame(){
 	bindCardClickEvent();
 	$('.fa-star').css('color', '#F4A460');
 };
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
  // Open and show the selected card.
 function showSymbol(card){
